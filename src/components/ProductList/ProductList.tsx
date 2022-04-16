474359c04537a50a6072f80e48b3e5dc25d50221
ffcabbar <ffcabbar@gmail.com>
2022-04-16 04:47:40 +0300

    updates productlist component for has no tags products

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -15,9 +15,13 @@ export const ProductList = ({ products }: Props) => {
             <div>
               <div className={styles.name}>{product.productName}</div>
               <div className={styles.tags}>
-                {product.tags.map((tag, index) => {
-                  return <Badge key={index}>{tag}</Badge>;
-                })}
+                {product.tags.length ? (
+                  product.tags.map((tag, index) => {
+                    return <Badge key={index}>{tag}</Badge>;
+                  })
+                ) : (
+                  <span className={styles.hasNoTags}>Product has no any tags</span>
+                )}
               </div>
             </div>
             <div className={styles.category}>{product.category}</div>
